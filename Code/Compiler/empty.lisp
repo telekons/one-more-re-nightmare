10d7e6fc76990eae45bd1d94eacb666ccdc09a8e
Hayley Patton <theemacsshibe@gmail.com>
2021-06-28 13:11:36 +1000

    Tweak re-empty-p so that it detects α[ε, ...] as empty

--- Code/Compiler/empty.lisp
+++ Code/Compiler/empty.lisp
@@ -4,7 +4,8 @@
   "Is a regular expression basically an empty string?
 This is different to NULLABLE, yes, NULLABLE would accept e.g. a* or anything that is a superset of { \"\" }, but this accepts only the empty string (± tags)."
   ((or (empty-string) (tag-set _)) t)
-  ((kleene r) (re-empty-p r))
+  ((or (kleene r) (alpha r _))
+   (re-empty-p r))
   ((or (join r s) (either r s) (both r s))
    (and (re-empty-p r) (re-empty-p s)))
   (_ nil))
