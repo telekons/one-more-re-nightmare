7988373208a0dcbbaa4de6fe0cfc7224e040a54a
Hayley Patton <theemacsshibe@gmail.com>
2022-04-16 10:20:02 +1000

    Ignore prototypes for GREP when unifying.

--- Code/DFA-construction/similar.lisp
+++ Code/DFA-construction/similar.lisp
@@ -61,10 +61,15 @@
             (assert-equivalent-sources s1 s2)
             (assert-equivalent (list v1 r1)
                                (list v2 r2))))
-  (((grep r1 s1) (grep r2 s2))
+  (((grep r1 p1) (grep r2 p2))
    (%similar r1 r2)
-   (%similar s1 s2))
+   ;; We don't actually need to do anything for GREP prototypes; they
+   ;; never cause effects, and they never change while generating a
+   ;; DFA. Unifying them just causes unnecessary failures.
+   #+(or)
+   (%similar p1 p2))
   (((alpha r1 n1) (alpha r2 n2))
+   ;; Though we do need to unify the history/nullable part of ALPHA.
    (%similar r1 r2)
    (%similar n1 n2))
   ((_ _) (error 'no-match)))
